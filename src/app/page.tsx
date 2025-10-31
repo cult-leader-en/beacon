f7db82eb09877654fc3f38b04b30485246c4e1ff
En Canada <75579214+cult-leader-en@users.noreply.github.com>
2025-10-31 06:55:52 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
